8c56b34c29b60b240497105903bc439b341850e3
dhas <dhas@dhas.com>
2020-03-19 16:44:12 +0100

    train and test sets

--- activity/activity.py
+++ activity/activity.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy import fftpack
 import matplotlib.pyplot as plt
 from sklearn import tree
+from joblib import dump, load
 import graphviz 
 
 def get_energy(X):
@@ -22,12 +23,10 @@ def dir2dataset(root):
 		y.append(class_to_label[class_name])
 		col_names  = ['%s' % i for i in range(8)]
 		df = pd.read_csv(str(file), names=col_names, sep='\t')	
-		acc = df.loc[df['1'] == 'ACC'].iloc[:,2:5].to_numpy().astype(np.double)	
-		x.append(np.concatenate([np.mean(acc,axis=0),
-							np.min(acc,axis=0),
-							np.max(acc,axis=0),
-							np.std(acc,axis=0),						
-							get_energy(acc)]))
+		acc = df.loc[df['1'] == 'ACC'].iloc[:,2:5].to_numpy().astype(np.double)
+		acc = np.sqrt(np.sum(acc**2, axis=1)) - 9.8
+		x.append([np.mean(acc),np.min(acc), 
+				  np.max(acc), np.std(acc), get_energy(acc)])
 	x = np.vstack(x)
 	return x, y
 
@@ -52,26 +51,27 @@ train_root  = data_root/'train'
 one_phone   = False
 
 x_trn, y_trn = dir2dataset(train_root)
-fig, axs = plt.subplots(len(FEATS), 3, figsize=(20,10))
+fig, axs = plt.subplots(1, len(FEATS), figsize=(20,5))
 for i,feat in enumerate(FEATS):
-	axs[i,0].set_ylabel(feat)
-	for j in range(3):
-		ax = axs[i,j]
-		ax.scatter(y_trn, x_trn[:,(i*3+j)])
-		ax.set_xticks(range(3))
+	axs[i].set_title(feat)
+	axs[i].scatter(y_trn, x_trn[:,i])
+	axs[i].set_xticks(range(3))
 fig.savefig('1_features.png')
 
-dtree = tree.DecisionTreeClassifier()
-dtree = dtree.fit(x_trn, y_trn)
-dot_data = tree.export_graphviz(dtree, 
-	class_names=list(class_to_label.keys()),
-	feature_names = DFEATS,
-	out_file=None) 
-graph = graphviz.Source(dot_data)
-if one_phone:
-	graph.render('activity_ddd') 
+if Path('dtree.joblib').exists():
+	dtree = load('dtree.joblib')
 else:
-	graph.render('activity')
+	dtree = tree.DecisionTreeClassifier()
+	dtree = dtree.fit(x_trn, y_trn)
+	dot_data = tree.export_graphviz(dtree, 
+		class_names=list(class_to_label.keys()),
+		feature_names = FEATS,
+		out_file=None) 
+	graph = graphviz.Source(dot_data)
+	if one_phone:
+		graph.render('activity_ddd') 
+	else:
+		graph.render('activity')
 
 
 test_root = data_root/'test'
